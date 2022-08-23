1795edc59e3ec27ec3391cae2314a774b0935104
Anitha Yoyak <anithacy2008@gmail.com>
2022-08-23 16:20:17 -0400

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { PromotionService } from './services/promotion.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -44,6 +45,7 @@ import { ContactComponent } from './contact/contact.component';
   ],
   providers: [
     DishService,
+    PromotionService,
   ],
   bootstrap: [AppComponent]
 })
