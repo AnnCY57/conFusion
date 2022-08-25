9d8c4bff04d41795fdc9d4945372daafa85afa06
Anitha Yoyak <anithacy2008@gmail.com>
2022-08-25 13:50:08 -0400

    Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -46,6 +47,7 @@ import { PromotionService } from './services/promotion.service';
   providers: [
     DishService,
     PromotionService,
+    LeaderService,
   ],
   bootstrap: [AppComponent]
 })
